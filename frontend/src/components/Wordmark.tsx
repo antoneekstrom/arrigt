053fbfcc75e16c7a2a3f322b679e7891be0babf4
Anton Ekström <anton.e.ekstrom@gmail.com>
2022-04-10 19:58:03 +0200

    Adds Wordmark component

---
+++ frontend/src/components/Wordmark.tsx
@@ -0,0 +1,11 @@
+/**
+ * The "logo" of the application.
+ */
+export function Wordmark() {
+  return (
+    <a href="/" className="text-lg font-medium">
+      arr<span className="text-turquoise-500">I</span>g
+      <span className="text-turquoise-500">T</span>
+    </a>
+  );
+}
